9c02dc092ac21abdd35bb389974ee9fe8c48d14f
Piotr Kasperski <piotr.kasperski94@gmail.com>
2024-08-16 01:46:03 +0200

    Auto-commit changes in submodule from branch 3-can-t-delete-shoplist

--- src/components/ShopLists/ShopListsContainer.tsx
+++ src/components/ShopLists/ShopListsContainer.tsx
@@ -1,98 +1,45 @@
-import {
-  IonContent,
-  IonFab,
-  IonFabButton,
-  IonIcon,
-  IonItem,
-  IonList,
-  IonPage,
-} from "@ionic/react";
-import React, { Dispatch, useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { IonContent, IonList, IonPage } from "@ionic/react";
 import ShopListContainer from "./ShopListContainer";
 import api from "../../Services/api";
-import { add } from "ionicons/icons";
-import AddShopListFab from "./AddShopListFab";
-import {
-  useStorage,
-  useStorageItem,
-} from "@capacitor-community/react-hooks/storage";
+import { useStorageItem } from "@capacitor-community/react-hooks/storage";
 import useProtectedApi from "../../hooks/useProtectedApi";
+import AddShopListFab from "./AddShopListFab";
 import ShopListModel from "../../models/ShopListsModel";
 
 function ShopListsContainer() {
-  const lists: Array<
-    [ShopListModel, React.Dispatch<React.SetStateAction<ShopListModel>>]
-  > = [[...useState<ShopListModel>({} as ShopListModel)]];
   const [shopLists, setShopLists] = useState<ShopListModel[]>([]);
-  const [token, setToken] = useStorageItem("access_token");
-  const { get } = useProtectedApi();
-  useEffect(() => {
-    const effect = async () =>
-      getShopLists().then((response) => {
-        console.log("getShopLists success");
-      });
-    effect();
-  }, []);
-  useEffect(() => {
-    const effect = async () => console.log("getShopLists success");
-    console.log(shopLists);
-    effect();
-  }, [shopLists]);
+  const { get, post } = useProtectedApi();
 
-  const config = {
-    headers: { Authorization: `Bearer ${token}` },
-  };
+  useEffect(() => {
+    get("shop-lists/")
+      .then(({ data }) => setShopLists(data))
+      .catch(console.log);
+  }, [get]);
+  useEffect(() => console.log("Updated shopLists:", shopLists), [shopLists]);
 
-  const getShopLists = async () => {
-    return await get("shop-lists/")
-      .then((response) => {
-        console.log(response.data);
-        setShopLists((list) => response.data);
-        return response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  const addNewShopList = (newShopListName: string) => {
-    api
-      .post("shop-lists/", { name: newShopListName }, config)
-      .then((response) => {
-        setShopLists([...shopLists, { ...response.data }]);
-        console.log("add shop list response", response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  const onDeleteList = (shopList: ShopListModel): void => {
-    setShopLists((shopListsState) => {
-      return shopListsState.splice(shopListsState.indexOf(shopList), 1);
-    });
-  };
+  const addNewShopList = async (name: string) =>
+    post("shop-lists/", { name })
+      .then(({ data }) => setShopLists((lists) => [...lists, data]))
+      .catch(console.log);
+
+  const onDeleteList = (shopList: ShopListModel) =>
+    setShopLists((lists) => lists.filter((item) => item !== shopList));
+
   return (
     <IonPage>
       <IonContent>
         <IonList>
-          {shopLists.map((shopList) => {
-            console.log(shopList);
-            return (
-              <ShopListContainer
-                shopList={shopList}
-                onDelete={(shopList) => {
-                  onDeleteList(shopList);
-                }}
-              />
-            );
-          })}
+          {shopLists.map((shopList) => (
+            <ShopListContainer
+              key={shopList.shopListID}
+              shopList={shopList}
+              onDelete={() => onDeleteList(shopList)}
+            />
+          ))}
         </IonList>
       </IonContent>
-      <AddShopListFab
-        onAddListClick={(newListName) => {
-          addNewShopList(newListName);
-          console.log(newListName);
-        }}
-      />
+      <AddShopListFab onAddListClick={(name) => addNewShopList(name)} />
     </IonPage>
   );
 }
