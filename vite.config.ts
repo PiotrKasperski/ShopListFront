6aca80376043508566674adeb3800248aa26a9e2
Piotr Kasperski <piotr.kasperski94@gmail.com>
2024-08-18 21:59:37 +0200

    Auto-commit changes in submodule from branch 9-test-implementation

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 /// <reference types="vitest" />
-import dts from "vite-plugin-dts";
 import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [dts({ exclude: "**/*.test.ts" }), react(), legacy()],
+  plugins: [react(), legacy()],
   test: {
     globals: true,
     environment: "jsdom",
