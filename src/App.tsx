b03528fb8108ca29fc519740a8fa2c38130701ec
Piotr Kasperski <piotr.kasperski94@gmail.com>
2024-08-19 15:44:13 +0200

    Auto-commit changes in submodule from branch 11-build-error

--- src/App.tsx
+++ src/App.tsx
@@ -76,10 +76,7 @@ const App: React.FC = () => {
   const handleOnRedirect = () => {
     setShowToast(true);
   };
-  const errorFallback = (error) => {
-    setMessage("ERROR");
-    return <></>;
-  };
+
   return (
     <IonApp>
       <IonHeader color="primary">
